cdddb2ee97a9b17791cc0b59b37a51cb7f29eb78
Ryan Friedman <ryanfriedman5410+github@gmail.com>
2023-10-21 19:25:40 -0500

    Port to ROS 2
    Signed-off-by: Ryan Friedman <ryanfriedman5410+github@gmail.com>

--- src/test_tif_loader.cpp
+++ src/test_tif_loader.cpp
@@ -61,7 +61,9 @@ class MapPublisher : public rclcpp::Node {
     }
   private:
     void timer_callback() {
-      auto msg = grid_map::GridMapRosConverter::toMessage(map_->getGridMap());
+      const auto now = node->now();
+      map.setTimestamp(now.nanoseconds());
+      const auto msg = grid_map::GridMapRosConverter::toMessage(map_->getGridMap());
       original_map_pub_->publish(std::move(msg));
     }
     rclcpp::TimerBase::SharedPtr timer_;
